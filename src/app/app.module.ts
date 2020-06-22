c3895a09d70d302a2259642cea20f7247350b299
prashe12 <prashil.lonakar@gmail.com>
2020-06-22 05:46:49 +0530

    Renderer Directive Which is beeter approach because directly playing with Dom is not good

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RendererDirective2Directive } from './directive/rendererDirective2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicDirective1Directive
+    BasicDirective1Directive,
+    RendererDirective2Directive
   ],
   imports: [
     BrowserModule
